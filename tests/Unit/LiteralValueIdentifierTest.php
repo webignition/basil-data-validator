73946b2173ad75b339f4b265a61417bc21030833
Jon Cram <jon@webignition.net>
2019-12-04 16:04:07 +0000

    Create LiteralValueIdentifier

---
+++ tests/Unit/LiteralValueIdentifierTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilDataValidator\Tests\Unit;
+
+use webignition\BasilDataValidator\LiteralValueIdentifier;
+
+class LiteralValueIdentifierTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var LiteralValueIdentifier
+     */
+    private $literalValueIdentifier;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->literalValueIdentifier = new LiteralValueIdentifier();
+    }
+
+    /**
+     * @dataProvider isDataProvider
+     */
+    public function testIs(string $value, bool $expectedIs)
+    {
+        $this->assertSame($this->literalValueIdentifier->is($value), $expectedIs);
+    }
+
+    public function isDataProvider(): array
+    {
+        return [
+            'empty' => [
+                'value' => '',
+                'expectedIs' => false,
+            ],
+            'whitespace' => [
+                'value' => '   ',
+                'expectedIs' => false,
+            ],
+            'unquoted' => [
+                'value' => 'value',
+                'expectedIs' => false,
+            ],
+            'no ending quote' => [
+                'value' => '"value',
+                'expectedIs' => false,
+            ],
+            'no starting quote' => [
+                'value' => 'value"',
+                'expectedIs' => false,
+            ],
+            'no ending quote; has escaped quotes' => [
+                'value' => '"va\"lu\"e\"',
+                'expectedIs' => false,
+            ],
+            'no starting quote; has escaped quotes' => [
+                'value' => '\"va\"lu\"e\""',
+                'expectedIs' => false,
+            ],
+            'quoted' => [
+                'value' => '"value"',
+                'expectedIs' => true,
+            ],
+            'quoted; has escaped quotes' => [
+                'value' => '"va\"lu\"e"',
+                'expectedIs' => true,
+            ],
+        ];
+    }
+}
