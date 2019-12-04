1506bf3a63cd97e3f2606d8507f7b834a10fe2d3
Jon Cram <webignition@gmail.com>
2019-12-04 18:55:59 +0000

    Validate actions (#16)

---
+++ src/Action/ActionValidator.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilDataValidator\Action;
+
+use webignition\BasilDataValidator\ResultType;
+use webignition\BasilDataValidator\ValueValidator;
+use webignition\BasilIdentifierAnalyser\IdentifierTypeAnalyser;
+use webignition\BasilModels\Action\ActionInterface;
+use webignition\BasilModels\Action\InputActionInterface;
+use webignition\BasilModels\Action\InteractionActionInterface;
+use webignition\BasilModels\Action\WaitActionInterface;
+use webignition\BasilValidationResult\InvalidResult;
+use webignition\BasilValidationResult\InvalidResultInterface;
+use webignition\BasilValidationResult\ResultInterface;
+use webignition\BasilValidationResult\ValidResult;
+
+class ActionValidator
+{
+    public const REASON_INVALID_TYPE = 'action-invalid-type';
+    public const REASON_INVALID_IDENTIFIER = 'action-invalid-identifier';
+    public const REASON_INVALID_VALUE = 'action-invalid-value';
+    private const VALID_TYPES = ['click', 'set', 'submit', 'wait', 'wait-for', 'back', 'forward', 'reload'];
+
+    private $identifierTypeAnalyser;
+    private $valueValidator;
+
+    public function __construct(IdentifierTypeAnalyser $identifierTypeAnalyser, ValueValidator $valueValidator)
+    {
+        $this->identifierTypeAnalyser = $identifierTypeAnalyser;
+        $this->valueValidator = $valueValidator;
+    }
+
+    public static function create(): ActionValidator
+    {
+        return new ActionValidator(
+            new IdentifierTypeAnalyser(),
+            ValueValidator::create()
+        );
+    }
+
+    public function validate(ActionInterface $action): ResultInterface
+    {
+        if ($action instanceof InteractionActionInterface) {
+            $identifier = $action->getIdentifier();
+
+            if (!$this->identifierTypeAnalyser->isElementIdentifier($identifier)) {
+                return new InvalidResult(
+                    $action,
+                    ResultType::ACTION,
+                    self::REASON_INVALID_IDENTIFIER
+                );
+            }
+        }
+
+        if ($action instanceof InputActionInterface) {
+            $valueValidationResult = $this->valueValidator->validate($action->getValue());
+
+            if ($valueValidationResult instanceof InvalidResultInterface) {
+                return new InvalidResult(
+                    $action,
+                    ResultType::ACTION,
+                    self::REASON_INVALID_VALUE,
+                    $valueValidationResult
+                );
+            }
+        }
+
+        if ($action instanceof WaitActionInterface) {
+            $value = $action->getDuration();
+            $value = ctype_digit($value) ? '"' . $value . '"' : $value;
+
+            $valueValidationResult = $this->valueValidator->validate($value);
+
+            if ($valueValidationResult instanceof InvalidResultInterface) {
+                return new InvalidResult(
+                    $action,
+                    ResultType::ACTION,
+                    self::REASON_INVALID_VALUE,
+                    $valueValidationResult
+                );
+            }
+        }
+
+        if (!in_array($action->getType(), self::VALID_TYPES)) {
+            return new InvalidResult(
+                $action,
+                ResultType::ACTION,
+                self::REASON_INVALID_TYPE
+            );
+        }
+
+        return new ValidResult($action);
+    }
+}
---
+++ tests/Unit/Action/ActionValidatorTest.php
@@ -0,0 +1,352 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilDataValidator\Tests\Unit\Action;
+
+use webignition\BasilDataValidator\Action\ActionValidator;
+use webignition\BasilDataValidator\ResultType;
+use webignition\BasilDataValidator\ValueValidator;
+use webignition\BasilModels\Action\ActionInterface;
+use webignition\BasilParser\ActionParser;
+use webignition\BasilValidationResult\InvalidResult;
+use webignition\BasilValidationResult\InvalidResultInterface;
+use webignition\BasilValidationResult\ValidResult;
+
+class ActionValidatorTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var ActionValidator
+     */
+    private $actionValidator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->actionValidator = ActionValidator::create();
+    }
+
+    /**
+     * @dataProvider validateIsValidDataProvider
+     */
+    public function testValidateIsValid(ActionInterface $action)
+    {
+        $this->assertEquals(new ValidResult($action), $this->actionValidator->validate($action));
+    }
+
+    public function validateIsValidDataProvider(): array
+    {
+        $actionParser = ActionParser::create();
+
+        return [
+            'click element identifier' => [
+                'action' => $actionParser->parse('click $".selector"'),
+            ],
+            'submit element identifier' => [
+                'action' => $actionParser->parse('submit $".selector"'),
+            ],
+            'wait-for element identifier' => [
+                'action' => $actionParser->parse('wait-for $".selector"'),
+            ],
+            'wait literal value (unquoted)' => [
+                'action' => $actionParser->parse('wait 1'),
+            ],
+            'wait literal value (quoted)' => [
+                'action' => $actionParser->parse('wait "1"'),
+            ],
+            'wait element identifier value' => [
+                'action' => $actionParser->parse('wait $".selector"'),
+            ],
+            'wait attribute identifier value' => [
+                'action' => $actionParser->parse('wait $".selector".attribute'),
+            ],
+            'wait browser size value' => [
+                'action' => $actionParser->parse('wait $browser.size'),
+            ],
+            'wait page title value' => [
+                'action' => $actionParser->parse('wait $page.title'),
+            ],
+            'wait page url value' => [
+                'action' => $actionParser->parse('wait $page.url'),
+            ],
+            'wait data parameter value' => [
+                'action' => $actionParser->parse('wait $data.key'),
+            ],
+            'wait environment parameter value' => [
+                'action' => $actionParser->parse('wait $env.KEY'),
+            ],
+            'set; literal value' => [
+                'action' => $actionParser->parse('set $".selector" to "value"'),
+            ],
+            'set; element identifier value' => [
+                'action' => $actionParser->parse('set $".selector" to $".value"'),
+            ],
+            'set; attribute identifier value' => [
+                'action' => $actionParser->parse('set $".selector" to $".element".attribute'),
+            ],
+            'set; browser size value' => [
+                'action' => $actionParser->parse('set $".selector" to $browser.size'),
+            ],
+            'set; page title value' => [
+                'action' => $actionParser->parse('set $".selector" to $page.title'),
+            ],
+            'set; page url value' => [
+                'action' => $actionParser->parse('set $".selector" to $page.url'),
+            ],
+            'set; data parameter value' => [
+                'action' => $actionParser->parse('set $".selector" to $data.key'),
+            ],
+            'set; environment parameter value' => [
+                'action' => $actionParser->parse('set $".selector" to $env.KEY'),
+            ],
+            'reload, no args' => [
+                'action' => $actionParser->parse('reload'),
+            ],
+            'back, no args' => [
+                'action' => $actionParser->parse('back'),
+            ],
+            'forward, no args' => [
+                'action' => $actionParser->parse('forward'),
+            ],
+            'reload, with args' => [
+                'action' => $actionParser->parse('reload arg1 arg2'),
+            ],
+            'back, with args' => [
+                'action' => $actionParser->parse('back arg1 arg2'),
+            ],
+            'forward, with args' => [
+                'action' => $actionParser->parse('forward arg1 arg2'),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider invalidInteractionActionDataProvider
+     * @dataProvider invalidInputActionDataProvider
+     * @dataProvider invalidWaitActionDataProvider
+     * @dataProvider invalidActionTypeDataProvider
+     */
+    public function testValidateNotValid(ActionInterface $action, InvalidResultInterface $expectedResult)
+    {
+        $this->assertEquals($expectedResult, $this->actionValidator->validate($action));
+    }
+
+    public function invalidInteractionActionDataProvider(): array
+    {
+        $actionParser = ActionParser::create();
+
+        return [
+            'interaction action: identifier invalid (element reference)' => [
+                'action' => $actionParser->parse('click $elements.element_name'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('click $elements.element_name'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'interaction action: identifier invalid (attribute reference)' => [
+                'action' => $actionParser->parse('click $elements.element_name.attribute_name'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('click $elements.element_name.attribute_name'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'interaction action: identifier invalid (attribute identifier)' => [
+                'action' => $actionParser->parse('click $".selector".attribute_name'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('click $".selector".attribute_name'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'interaction action: identifier invalid (page element reference)' => [
+                'action' => $actionParser->parse('click $page_import_name.elements.element_name'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('click $page_import_name.elements.element_name'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'interaction action: identifier invalid (browser property)' => [
+                'action' => $actionParser->parse('click $browser.size'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('click $browser.size'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'interaction action: identifier invalid (page property)' => [
+                'action' => $actionParser->parse('click $page.url'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('click $page.url'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'interaction action: identifier invalid (data parameter)' => [
+                'action' => $actionParser->parse('click $data.key'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('click $data.key'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'interaction action: identifier invalid (environment parameter)' => [
+                'action' => $actionParser->parse('click $env.KEY'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('click $env.KEY'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'interaction action: identifier invalid (quoted literal)' => [
+                'action' => $actionParser->parse('click "selector"'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('click "selector"'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'interaction action: identifier invalid (literal)' => [
+                'action' => $actionParser->parse('click selector'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('click selector'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+        ];
+    }
+
+    public function invalidInputActionDataProvider(): array
+    {
+        $actionParser = ActionParser::create();
+
+        return [
+            'input action: identifier invalid (element reference)' => [
+                'action' => $actionParser->parse('set $elements.element_name to "value"'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('set $elements.element_name to "value"'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'input action: identifier invalid (attribute reference)' => [
+                'action' => $actionParser->parse('set $elements.element_name.attribute_name to "value"'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('set $elements.element_name.attribute_name to "value"'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'input action: identifier invalid (attribute identifier)' => [
+                'action' => $actionParser->parse('set $".selector".attribute_name to "value"'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('set $".selector".attribute_name to "value"'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'input action: identifier invalid (page element reference)' => [
+                'action' => $actionParser->parse('set $page_import_name.elements.element_name to "value"'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('set $page_import_name.elements.element_name to "value"'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'input action: identifier invalid (browser property)' => [
+                'action' => $actionParser->parse('set $browser.size to "value"'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('set $browser.size to "value"'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'input action: identifier invalid (page property)' => [
+                'action' => $actionParser->parse('set $page.url to "value"'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('set $page.url to "value"'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'input action: identifier invalid (data parameter)' => [
+                'action' => $actionParser->parse('set $data.key to "value"'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('set $data.key to "value"'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'input action: identifier invalid (environment parameter)' => [
+                'action' => $actionParser->parse('set $env.KEY to "value"'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('set $env.KEY to "value"'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'input action: identifier invalid (quoted literal)' => [
+                'action' => $actionParser->parse('set "selector" to "value"'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('set "selector" to "value"'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_IDENTIFIER
+                ),
+            ],
+            'input action: value invalid (unquoted value)' => [
+                'action' => $actionParser->parse('set $".selector" to $page.address'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('set $".selector" to $page.address'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_VALUE,
+                    new InvalidResult(
+                        '$page.address',
+                        ResultType::VALUE,
+                        ValueValidator::REASON_PROPERTY_INVALID
+                    )
+                ),
+            ],
+        ];
+    }
+
+    public function invalidWaitActionDataProvider(): array
+    {
+        $actionParser = ActionParser::create();
+
+        return [
+            'wait action: value invalid (element reference)' => [
+                'action' => $actionParser->parse('wait $elements.element_name'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('wait $elements.element_name'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_VALUE,
+                    new InvalidResult(
+                        '$elements.element_name',
+                        ResultType::VALUE,
+                        ValueValidator::REASON_INVALID
+                    )
+                ),
+            ],
+        ];
+    }
+
+    public function invalidActionTypeDataProvider(): array
+    {
+        $actionParser = ActionParser::create();
+
+        return [
+            'invalid action type' => [
+                'action' => $actionParser->parse('invalid'),
+                'expectedResult' => new InvalidResult(
+                    $actionParser->parse('invalid'),
+                    ResultType::ACTION,
+                    ActionValidator::REASON_INVALID_TYPE
+                ),
+            ],
+        ];
+    }
+}
