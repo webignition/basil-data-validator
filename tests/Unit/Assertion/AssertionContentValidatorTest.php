c0155055198aca79df7e09fea7cbb195fb9c1678
Jon Cram <webignition@gmail.com>
2019-12-05 12:07:08 +0000

    Validate assertions (#17)

---
+++ tests/Unit/Assertion/AssertionContentValidatorTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilDataValidator\Tests\Unit\Assertion;
+
+use webignition\BasilDataValidator\Assertion\AssertionContentValidator;
+use webignition\BasilDataValidator\ResultType;
+use webignition\BasilDataValidator\Tests\DataProvider\ValueDataProviderTrait;
+use webignition\BasilValidationResult\InvalidResult;
+use webignition\BasilValidationResult\ValidResult;
+
+class AssertionContentValidatorTest extends \PHPUnit\Framework\TestCase
+{
+    use ValueDataProviderTrait;
+
+    /**
+     * @var AssertionContentValidator
+     */
+    private $validator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->validator = AssertionContentValidator::create();
+    }
+
+    /**
+     * @dataProvider invalidValueDataProvider
+     */
+    public function testValidateNotValid(string $value, string $expectedReason)
+    {
+        $expectedResult = new InvalidResult($value, ResultType::VALUE, $expectedReason);
+
+        $this->assertEquals($expectedResult, $this->validator->validate($value));
+    }
+
+    /**
+     * @dataProvider validValueDataProvider
+     * @dataProvider validAssertionValueDataProvider
+     */
+    public function testValidateIsValid(string $value)
+    {
+        $expectedResult = new ValidResult($value);
+
+        $this->assertEquals($expectedResult, $this->validator->validate($value));
+    }
+
+    public function validAssertionValueDataProvider(): array
+    {
+        return [
+            'descendant element dom identifier' => [
+                'value' => '$"{{ parent }} .selector"',
+            ],
+            'descendant attribute dom identifier' => [
+                'value' => '$"{{ parent }} .selector".attribute_name',
+            ],
+        ];
+    }
+}
--- tests/Unit/ValueValidatorTest.php
+++ tests/Unit/ValueValidatorTest.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace webignition\BasilDataValidator\Tests\Unit;
 
 use webignition\BasilDataValidator\ResultType;
+use webignition\BasilDataValidator\Tests\DataProvider\ValueDataProviderTrait;
 use webignition\BasilDataValidator\ValueValidator;
 use webignition\BasilValidationResult\InvalidResult;
 use webignition\BasilValidationResult\ValidResult;
 
 class ValueValidatorTest extends \PHPUnit\Framework\TestCase
 {
+    use ValueDataProviderTrait;
+
     /**
      * @var ValueValidator
      */
@@ -24,7 +27,7 @@ class ValueValidatorTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @dataProvider validateNotValidDataProvider
+     * @dataProvider invalidValueDataProvider
      */
     public function testValidateNotValid(string $value, string $expectedReason)
     {
@@ -33,68 +36,13 @@ class ValueValidatorTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($expectedResult, $this->valueValidator->validate($value));
     }
 
-    public function validateNotValidDataProvider(): array
-    {
-        return [
-            'invalid page property' => [
-                'value' => '$page.foo',
-                'expectedReason' => ValueValidator::REASON_PROPERTY_INVALID,
-            ],
-            'invalid browser property name' => [
-                'value' => '$browser.foo',
-                'expectedReason' => ValueValidator::REASON_PROPERTY_INVALID,
-            ],
-            'unquoted' => [
-                'value' => 'value',
-                'expectedReason' => ValueValidator::REASON_INVALID,
-            ],
-            'misquoted' => [
-                'value' => '"value',
-                'expectedReason' => ValueValidator::REASON_INVALID,
-            ],
-        ];
-    }
-
     /**
-     * @dataProvider validateIsValidDataProvider
+     * @dataProvider validValueDataProvider
      */
     public function testValidateIsValid(string $value)
     {
         $expectedResult = new ValidResult($value);
 
         $this->assertEquals($expectedResult, $this->valueValidator->validate($value));
-    }
-
-    public function validateIsValidDataProvider(): array
-    {
-        return [
-            'quoted literal' => [
-                'value' => '"value"',
-            ],
-            'escaped quoted literal' => [
-                'value' => '"va\"l\"ue"',
-            ],
-            'data parameter' => [
-                'value' => '$data.value',
-            ],
-            'page property, url' => [
-                'value' => '$page.url',
-            ],
-            'page property, title' => [
-                'value' => '$page.title',
-            ],
-            'browser property, size' => [
-                'value' => '$browser.size',
-            ],
-            'element dom identifier' => [
-                'value' => '$".selector"',
-            ],
-            'attribute dom identifier' => [
-                'value' => '$".selector".attribute_name',
-            ],
-            'environment parameter' => [
-                'value' => '$env.KEY',
-            ],
-        ];
     }
 }
